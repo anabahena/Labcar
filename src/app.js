c965a001ae89207c9e7ee57f44ae7bba525045b3
AnaBahena <ana-bahena@live.com.mx>
2019-07-22 16:19:49 -0500

    Subiendo la página con las secciones requeridas, únicamente para mobile

--- src/app.js
+++ src/app.js
@@ -5,8 +5,17 @@ let contador =1;
 function main() {
 $('.menuDes').click (function(){
     // $('nav').toggle();
-
-
+if (contador == 1){
+    $('nav').animate({
+        left:'0'
+    });
+    contador = 0;
+} else {
+    contador = 1;
+    $('nav').animate({
+        left:'-100%'
+});
+}
 });
 
 
